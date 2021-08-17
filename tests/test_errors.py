b29401f1c2627f86d2aa31aca5f0419ce8084a5f
Sondre Benjamin Aasen <eleljrk@gmail.com>
2021-08-17 17:20:39 +0200

    Fixed broken test (test_record_deletion_failure)

--- tests/test_errors.py
+++ tests/test_errors.py
@@ -52,52 +52,53 @@ class TestErrors(unittest.TestCase):
         errno = 0
         message = "Record does not exist: /home/a2j/records/does-not-exist"
 
-        self._test_error(path, errno, message)
+        self._test_error(path, errno, message, self.client.get)
 
     def test_record_direct_path_injection(self):
         path = "/record/../injection.py/version/"
         errno = 0
         message = "Record does not exist: /home/a2j/records/.."
 
-        self._test_error(path, errno, message)
+        self._test_error(path, errno, message, self.client.get)
 
     def test_record_traversal_encoding_path_injection(self):
         path = "/record/%2e%2e%2finjection.py/version/"
         errno = 0
         message = "Record does not exist: /home/a2j/records/.."
 
-        self._test_error(path, errno, message)
+        self._test_error(path, errno, message, self.client.get)
 
     def test_record_deletion_failure(self):
         path = "/record/does-not-exist/"
         errno = 0
         message = "Record does not exist: /home/a2j/records/does-not-exist"
 
-        self._test_error(path, errno, message)
+        self._test_error(path, errno, message, self.client.delete)
 
     def test_command_does_not_exist(self):
         path = "/record/test.mgz/not-real/fake-command/123/version/"
         errno = 1
         message = "Invalid commands: ['123', 'fake-command', 'not-real']"
 
-        self._test_error(path, errno, message)
+        self._test_error(path, errno, message, self.client.get)
 
     def test_commands_are_empty(self):
         path = "/record/test.mgz/"
         errno = 3
         message = "No commands received."
 
-        self._test_error(path, errno, message)
+        self._test_error(path, errno, message, self.client.get)
 
     def test_minimap_does_not_exist(self):
         path = "/minimap/does-not-exist/not-tested/"
         errno = 0
         message = "Record does not exist: /home/a2j/records/does-not-exist"
 
-        self._test_error(path, errno, message)
+        self._test_error(path, errno, message, self.client.get)
 
-    def _test_error(self, path: str, errno: int, message: str):
-        data = self.client.get(path).get_json()
+    @staticmethod
+    def _test_error(path: str, errno: int, message: str, function: callable):
+        data = function(path).get_json()
 
         assert len(data["errors"]) == 1
         assert data["errors"][0]["errno"] == errno
