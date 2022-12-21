b51c693292cfd9e369e574b04b8bedb547817392
Sondre Benjamin Aasen <sondre.a@journeyagency.com>
2022-12-21 20:47:06 +0100

    Updated prettifier script; Updated match and summary results after mgz update

--- tests/data/prettier.py
+++ tests/data/prettier.py
@@ -1,25 +1,37 @@
 import json
 from pathlib import Path
 
+PREFIX = "temp_"
+SUFFIX = ".json"
 
-def prettier(name):
-    with open(Path(__file__).parent / (name + ".json"), "r") as read_file:
-        with open(Path(__file__).parent / ("prettier_" + name + ".json"), "w") as write_file:
-            write_file.write(json.dumps(json.loads(read_file.read()), indent=2, sort_keys=True))
+
+def prettier(name: str):
+    with open(Path(__file__).parent / (name + SUFFIX), "r") as file:
+        raw_json = json.loads(file.read())
+
+    with open(Path(__file__).parent / (PREFIX + name + SUFFIX), "w") as file:
+        pretty_json = json.dumps(
+            raw_json,
+            indent=2,
+            sort_keys=True,
+        )
+
+        file.write(pretty_json)
+
+
+def overwrite(name: str):
+    path = Path(__file__).parent / (PREFIX + name + SUFFIX)
+
+    with open(path, "r") as file:
+        pretty_json = file.read()
+
+    with open(Path(__file__).parent / (name + SUFFIX), "w") as file:
+        file.write(pretty_json)
+        path.unlink()
 
 
 prettier("summary")
 prettier("match")
-
-
-def overwrite(name):
-    read_path = Path(__file__).parent / ("prettier_" + name + ".json")
-
-    with open(read_path, "r") as read_file:
-        with open(Path(__file__).parent / (name + ".json"), "w") as write_file:
-            write_file.write(read_file.read())
-            read_path.unlink()
-
 
 overwrite("summary")
 overwrite("match")
